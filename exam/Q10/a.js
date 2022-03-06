eacb6efac42d55de6844111c47c7bdeb6c6776cf
shadidabbasoptimum <shadi.dabbas@optimumpartners.co>
2022-03-05 19:54:14 -0600

    .

--- exam/Q10/a.js
+++ exam/Q10/a.js
@@ -11,9 +11,3 @@ const x = findFirstN(5,[4,5,3,6]);
 console.log(x)
 
 
-console.log("10- const findFirstN = (n , arr) => { \n if(n > arr.length){ \n return 'Error: Insufficient items'; \n } \n else{ \n return arr.find(m => n==m); \n }\n} \n const x = findFirstN(5,[4,5,3,6]); \n console.log(x)");
-console.log("1- true => its an inline element, if you put  and  beside each other they will be at the same line \n 2- false \n 3- false 4- true 5- false");
-console.log("6- 18, because of Closures that bind a loop variable have this bug we can fix it by using let instead of var");
-console.log("7- 12, no closure here and 4 * 3 = 12");
-console.log("8- 18, because f will take C values which are (a,b,c) and then he make b = a which means b was 19 but now it 18");
-console.log("شوف كيف بدخن");
