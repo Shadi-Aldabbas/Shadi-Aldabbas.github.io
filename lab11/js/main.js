5311faad3f8c030ea562cff4fcce67e2f0b2474d
shadidabbasoptimum <shadi.dabbas@optimumpartners.co>
2022-03-11 11:15:10 -0600

    .

--- lab11/js/main.js
+++ lab11/js/main.js
@@ -2,76 +2,133 @@
 
 import { Patient } from "./module/Patient.js";
 
-
-
 $(document).ready(function () {
-  const patientData = [];
+    const getAge = (dateString) => {
+        let today = new Date();
+        let birthDate = new Date(dateString);
+        let age = today.getFullYear() - birthDate.getFullYear();
+        let m = today.getMonth() - birthDate.getMonth();
+        if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+            age--;
+        }
+        return age;
+    }
+    
+    let patientData = [];
 
-  $("#patientForm").submit(function (event) {
-    event.preventDefault();
-    const ID = $("#ID").val();
-    const Fname = $("#Fname").val();
-    const Mname = $("#Mname").val();
-    const Lname = $("#Lname").val();
-    const dateOfBirth = $("#dateOfBirth").val();
-    const dep = $("#dep").val();
-    const OutPatientYes = $("#OutPatientYes").val();
-    const OutPatientNo = $("#OutPatientNo").val();
+    $("#patientForm").submit(function (event) {
+        event.preventDefault();
+        const ID = $("#ID").val();
+        const Fname = $("#Fname").val();
+        const Mname = $("#Mname").val();
+        const Lname = $("#Lname").val();
+        const dateOfBirth = $("#dateOfBirth").val();
+        const dep = $("#dep").val();
+        const OutPatientYes = $("#OutPatientYes").is(':checked');
+        const OutPatientNo = $("#OutPatientNo").is(':checked');
 
-    const newPatient = new Patient(ID,Fname,Mname,Lname,dateOfBirth,dep,OutPatientYes,OutPatientNo);
-    patientData.push(newPatient);
-    createPatient(newPatient);
-    // $("#ID").val("");
-    // $("#Fname").val("");
-    // $("#Mname").val("");
-    // $("#Lname").val("");
-    // $("#dateOfBirth").val("");
-    // $("#dep").val("");
-    // $("#OutPatientYes").val("");
-    // $("#OutPatientNo").val("");
-  });
+        const newPatient = new Patient(ID, Fname, Mname, Lname, dateOfBirth, dep, OutPatientYes, OutPatientNo);
+        patientData.push(newPatient);
+        createPatient(newPatient);
+        $("#ID").val("");
+        $("#Fname").val("");
+        $("#Mname").val("");
+        $("#Lname").val("");
+        $("#dateOfBirth").val("");
+        $("#dep").val("");
+        $("#OutPatientYes").val("");
+        $("#OutPatientNo").val("");
+    });
 
-//   function createAccount(newPatient, numAccounts) {
-//     const tblAccounts = document.querySelector("#tblAccounts");
-//     const newRow = tblAccounts.insertRow(-1);
-//     const newCellRowId = newRow.insertCell(0);
-//     const nextRowId = numAccounts + 2;
-//     const strNewRowId = document.createTextNode(`${nextRowId}.`);
-//     newCellRowId.appendChild(strNewRowId); // a
-//     const newCellAccountNo = newRow.insertCell(1);
-//     const strNewRowAccountNo = document.createTextNode(
-//       `${newPatient.getAccountNo()}`
-//     );
-//     newCellAccountNo.appendChild(strNewRowAccountNo);
-//     const newCellCustomerName = newRow.insertCell(2);
-//     const strNewRowCustomerName = document.createTextNode(
-//       `${newPatient.getCustomerName()}`
-//     );
-//     newCellCustomerName.appendChild(strNewRowCustomerName);
-//     const newCellAccountType = newRow.insertCell(3);
-//     const strNewRowAccountType = document.createTextNode(
-//       `${newPatient.getAccountType()}`
-//     );
-//     newCellAccountType.appendChild(strNewRowAccountType);
-//     console.log("Account created");
-//   }
-const createPatient = (newPatient) => { 
-    const row_id = $('#tablePatients tbody tr').length + 1;
-    const is_out = newPatient.OutPatientYes == true ? true : false;
-    const row = `
-    <tr>
-        <td>${newPatient.getID()}</td>
-        <td>${newPatient.getFname()}</td>
-        <td>${newPatient.getMname()}</td>
-        <td>${newPatient.getLname()}</td>
-        <td>${newPatient.getdateOfBirth()}</td>
-        <td>${newPatient.getdep()}</td>
-        <td>${is_out}</td>
-        
-    </tr>
-    `
-    $('#tablePatients tbody').append(row);
-    // this.reset();
-    alert('paitent inserted successfully');
-}
+
+    const createPatient = (newPatient) => {
+        const is_out = newPatient.getOutPatientYes() == true ? "Yes" : "No";
+        const row = `<tr>
+                    <td>${newPatient.getID()}</td>
+                    <td>${newPatient.getFname()}</td>
+                    <td>${newPatient.getMname()}</td>
+                    <td>${newPatient.getLname()}</td>
+                    <td>${newPatient.getdateOfBirth()}</td>
+                    <td>${newPatient.getdep()}</td>
+                    <td>${is_out}</td>
+                </tr>`;
+        $('#tablePatients tbody').append(row);
+    }
+
+    $("#chkShowOutPatients").on("click", function (event) {
+        if ($('#chkShowOutPatients').is(':checked')) {
+            const newData = patientData.filter(x => x.getOutPatientYes() == true);
+            $('#tablePatients tbody').empty();
+            newData.forEach(patient => {
+                const is_out = patient.getOutPatientYes() == true ? "Yes" : "No";
+                const row = `<tr>
+                    <td>${patient.getID()}</td>
+                    <td>${patient.getFname()}</td>
+                    <td>${patient.getMname()}</td>
+                    <td>${patient.getLname()}</td>
+                    <td>${patient.getdateOfBirth()}</td>
+                    <td>${patient.getdep()}</td>
+                    <td>${is_out}</td>
+                </tr>`;
+                $('#tablePatients tbody').append(row);
+
+            });
+        } else {
+            const newData = patientData;
+            $('#tablePatients tbody').empty();
+            newData.forEach(patient => {
+                const is_out = patient.getOutPatientYes() == true ? "Yes" : "No";
+                const row = `<tr>
+                    <td>${patient.getID()}</td>
+                    <td>${patient.getFname()}</td>
+                    <td>${patient.getMname()}</td>
+                    <td>${patient.getLname()}</td>
+                    <td>${patient.getdateOfBirth()}</td>
+                    <td>${patient.getdep()}</td>
+                    <td>${is_out}</td>
+                </tr>`;
+                $('#tablePatients tbody').append(row);
+            });
+        }
+    });
+
+    $("#chkElderlyPatients").on("click", function (event) {
+        if ($('#chkElderlyPatients').is(':checked')) {
+            const newData = patientData.filter(x => getAge(x.getdateOfBirth()) > 2);
+            $('#tablePatients tbody').empty();
+            newData.forEach(patient => {
+                const is_out = patient.getOutPatientYes() == true ? "Yes" : "No";
+                const row = `<tr>
+                    <td>${patient.getID()}</td>
+                    <td>${patient.getFname()}</td>
+                    <td>${patient.getMname()}</td>
+                    <td>${patient.getLname()}</td>
+                    <td>${patient.getdateOfBirth()}</td>
+                    <td>${patient.getdep()}</td>
+                    <td>${is_out}</td>
+                </tr>`;
+                $('#tablePatients tbody').append(row);
+
+            });
+        } else {
+            const newData = patientData;
+            $('#tablePatients tbody').empty();
+            newData.forEach(patient => {
+                const is_out = patient.getOutPatientYes() == true ? "Yes" : "No";
+                const row = `<tr>
+                    <td>${patient.getID()}</td>
+                    <td>${patient.getFname()}</td>
+                    <td>${patient.getMname()}</td>
+                    <td>${patient.getLname()}</td>
+                    <td>${patient.getdateOfBirth()}</td>
+                    <td>${patient.getdep()}</td>
+                    <td>${is_out}</td>
+                </tr>`;
+                $('#tablePatients tbody').append(row);
+            });
+        }
+    });
+
 });
+
+
--- lab11/js/module/Patient.js
+++ lab11/js/module/Patient.js
@@ -1,45 +1,45 @@
 "use strict";
 
 export class Patient {
-  #ID = null;
-  #Fname = null;
-  #Mname = null;
-  #Lname = null;
-  #dateOfBirth = null;
-  #dep = null;
-  #OutPatientYes = null;
-  #OutPatientNo = null;
+    #ID = null;
+    #Fname = null;
+    #Mname = null;
+    #Lname = null;
+    #dateOfBirth = null;
+    #dep = null;
+    #OutPatientYes = null;
+    #OutPatientNo = null;
 
-  constructor(ID, Fname, Mname, Lname, dateOfBirth, dep, OutPatientYes,OutPatientNo) {
-    this.#ID = ID;
-    this.#Fname = Fname;
-    this.#Mname = Mname;
-    this.#Lname = Lname;
-    this.#dateOfBirth = dateOfBirth;
-    this.#dep = dep;
-    this.#OutPatientYes = OutPatientYes;
-    this.#OutPatientNo = OutPatientNo;
-  }
+    constructor(ID, Fname, Mname, Lname, dateOfBirth, dep, OutPatientYes, OutPatientNo) {
+        this.#ID = ID;
+        this.#Fname = Fname;
+        this.#Mname = Mname;
+        this.#Lname = Lname;
+        this.#dateOfBirth = dateOfBirth;
+        this.#dep = dep;
+        this.#OutPatientYes = OutPatientYes;
+        this.#OutPatientNo = OutPatientNo;
+    }
 
-  getID = () => this.#ID;
-  getFname = () => this.#Fname;
-  getMname = () => this.#Mname;
-  getLname = () => this.#Lname;
-  getdateOfBirth = () => this.#dateOfBirth;
-  getdep = () => this.#dep;
-  getOutPatientYes = () => this.#OutPatientYes;
-  getOutPatientNo = () => this.#OutPatientNo;
+    getID = () => this.#ID;
+    getFname = () => this.#Fname;
+    getMname = () => this.#Mname;
+    getLname = () => this.#Lname;
+    getdateOfBirth = () => this.#dateOfBirth;
+    getdep = () => this.#dep;
+    getOutPatientYes = () => this.#OutPatientYes;
+    getOutPatientNo = () => this.#OutPatientNo;
 
-  setID = (ID) => (this.#ID = ID);
-  setFname = (Fname) => (this.#Fname = Fname);
-  setMname = (Mname) => (this.#Mname = Mname);
-  setLname = (Lname) => (this.#Lname = Lname);
-  setdateOfBirth = (dateOfBirth) => (this.#dateOfBirth = dateOfBirth);
-  setdep = (dep) => (this.#dep = dep);
-  setOutPatientYes = (OutPatient) => (this.#OutPatientYes = OutPatient);
-  setOutPatientNo = (OutPatient) => (this.#OutPatientNo = OutPatient);
+    setID = (ID) => (this.#ID = ID);
+    setFname = (Fname) => (this.#Fname = Fname);
+    setMname = (Mname) => (this.#Mname = Mname);
+    setLname = (Lname) => (this.#Lname = Lname);
+    setdateOfBirth = (dateOfBirth) => (this.#dateOfBirth = dateOfBirth);
+    setdep = (dep) => (this.#dep = dep);
+    setOutPatientYes = (OutPatient) => (this.#OutPatientYes = OutPatient);
+    setOutPatientNo = (OutPatient) => (this.#OutPatientNo = OutPatient);
 
-  toString = () => {
-    return ` ID: ${this.ID} Fname: ${this.Fname} Mname: ${this.Mname} Lname: ${this.Lname} dateOfBirth: ${this.dateOfBirth} dep: ${this.dep} OutPatientYes: ${this.OutPatientYes} OutPatientNo: ${this.OutPatientNo}`
-  }
+    toString = () => {
+        return ` ID: ${this.getID()} Fname: ${this.getFname()} Mname: ${this.getMname()} Lname: ${this.getLname()} dateOfBirth: ${this.getdateOfBirth()} dep: ${this.getdep()} OutPatientYes: ${this.getOutPatientYes()} OutPatientNo: ${this.getOutPatientNo()}`
+    }
 }
