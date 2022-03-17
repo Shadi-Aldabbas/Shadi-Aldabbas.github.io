94552d96c07fa058c2707d83026dc0b6a0382374
shadidabbasoptimum <shadi.dabbas@optimumpartners.co>
2022-03-17 10:21:20 -0500

    1234

---
+++ final exam/event-delegation-bank-app/JS/src.js
@@ -0,0 +1,15 @@
+/*  Attach a directly bound event handler */
+$("#container a").on("click", function(event) {
+
+    event.preventDefault(); 
+    $("#container").append(`
+    <section id="articlesList">
+        <article>
+            <p>Here is an Account - 
+                <a id="me" href="">Click to Add a new Account</a>
+            </p>
+        </article>
+    </section>
+`);
+});
+  
