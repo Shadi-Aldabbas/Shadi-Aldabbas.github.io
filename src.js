0819b80e4d1d0b8969e82b5bb3b48ecbd4f6efc9
shadidabbasoptimum <shadi.dabbas@optimumpartners.co>
2022-03-17 12:04:50 -0500

    qwdfghgfds

--- src.js
+++ src.js
@@ -1,1 +1,3 @@
 console.log(`option C is not valid because its not valid first name is not a valid name they should put it in "" so js can accept it`);
+
+console.log("you need this ?!:   https://github.com/Shadi-Aldabbas/Shadi-Aldabbas.github.io/tree/main/final%20exam");
