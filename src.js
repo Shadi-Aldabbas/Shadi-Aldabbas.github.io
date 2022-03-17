69a5a6654fe51f7d25c7f461b5f01b6d6173e86b
shadidabbasoptimum <shadi.dabbas@optimumpartners.co>
2022-03-17 12:10:02 -0500

    sdfgh,

--- src.js
+++ src.js
@@ -1,3 +1,7 @@
 console.log(`option C is not valid because its not valid first name is not a valid name they should put it in "" so js can accept it`);
 
 console.log("you need this ?!:   https://github.com/Shadi-Aldabbas/Shadi-Aldabbas.github.io/tree/main/final%20exam");
+
+
+console.log("to run student regestration app use:     npm run serve");
+console.log("don't forget to add jquery lib in the delegation question it will fail if u didn't copy and paste it from here :      https://github.com/Shadi-Aldabbas/Shadi-Aldabbas.github.io/blob/main/final%20exam/event-delegation-bank-app/index.html");
